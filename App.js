6eb1dc8ba0da2e83aa91a0ade1aebe46bc01e823
Juliano Gaspar <julianogaspar@gmail.com>
2022-05-22 16:39:39 -0300

    commit inicial

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Contacts from './src/pages/contats'
+import Information from './src/pages/information'
+import Home from './src/pages/home'
+import Other from './src/pages/other'
+import { FontAwesome } from '@expo/vector-icons'
+import 'react-native-gesture-handler';
+
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+
+function Tabs() {
+  return (
+    <Tab.Navigator
+      // screenOptions={{ headerShown: false }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName = 'home';
+          switch (route.name) {
+            case 'Home':
+              iconName = 'home'
+              break;
+            case 'Contatos':
+              iconName = 'users'
+              break;
+            case 'Mais...':
+              iconName = 'ellipsis-h'
+              break;
+          }
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name='Home' component={Home} />
+      <Tab.Screen name='Contatos' component={Contacts} />
+      <Tab.Screen name='Mais...' component={Other} />
+    </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='Principal' component={Tabs} />
+        <Stack.Screen name='Information' component={Information} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// function NavigationByStack() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Contacts" component={Contacts} />
+//       <Stack.Screen name="Information" component={Information} />
+//     </Stack.Navigator>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator>
+//         <Tab.Screen name='Home' component={Home} />
+//         <Tab.Screen name='Contatos' component={NavigationByStack} />
+//         <Tab.Screen name='Mais...' component={Other} />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   )
+// }
---
+++ Readme.md
@@ -0,0 +1,17 @@
+![App Screenshot](.github/cover.png)
+
+# App Navegação com Biblioteca Elements
+Projeto com exemplo de navegação com React-Native e utilizando a biblioteca de Elements com componentes de interface.
+Projeto criado no contexto da disciplina de desenvolvimento mobile.
+
+
+## Assuntos abordados
+
+- React Native
+- Expo
+- React Navigation
+- Navigation Stack
+- Navigation Tabs
+- Navigation Drawer
+- Routes
+- React Native Elements
